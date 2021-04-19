722c651d878351cc085caa926dcc2726618128a3
John Carlo Cunanan <codingjohn@outlook.com>
2021-04-19 12:03:34 +0800

    Create a Nav components
    Installed styled-components
    Set body's font-family to Poppins
    Created a responsive nav component

---
+++ components/Nav/navstyled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Column = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Logo = styled.span`
+  display: inline-block;
+  font-family: "Poppins", sans-serif;
+  font-size: 1.5rem;
+  line-height: 2.25rem;
+  font-weight: 700;
+  color: rgba(40, 37, 56, 1);
+`;
+
+export const Unbold = styled.span`
+  font-weight: 300;
+`;
