a1fa35450a85da83497ae82ac725ce9f4129a264
John Carlo Cunanan <codingjohn@outlook.com>
2021-04-19 12:08:18 +0800

    Change px to rem, Remove Nav component on pages index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Nav from "../components/Nav";
 
 export default function Home() {
   return (
@@ -9,7 +8,6 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Nav />
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
