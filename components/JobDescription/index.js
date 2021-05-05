0d73e9da1a01bc13ffa3ee6b683a675fab173197
John Carlo Cunanan <codingjohn@outlook.com>
2021-05-05 20:17:33 +0800

    Change company logo styling

--- components/JobDescription/index.js
+++ components/JobDescription/index.js
@@ -1,23 +1,27 @@
 import style from "./jobDescription.module.scss";
-import { job } from "../../utils/fakeData";
 import { TimeIcon, WorldIcon } from "./Icon";
+import moment from "moment";
 
-const JobDescription = () => {
+const JobDescription = ({ job }) => {
   return (
     <section className={style.description}>
       <div className={style.row1}>
         <div className={style.column}>
-          <h2 className={style.h2}>{job.position}</h2>
+          <h2 className={style.h2}>{job.title}</h2>
           <span className={style.type}>{job.type}</span>
           <span className={style.time}>
             <TimeIcon />
-            {job.timePosted}
+            {moment(job.created_at).fromNow()}
           </span>
         </div>
       </div>
       <div className={style.row2}>
         <div className={style.column}>
-          <img src={job.image} alt="company logo" className={style.logo} />
+          <div
+            style={{ backgroundImage: `url(${job.company_logo})` }}
+            alt="company logo"
+            className={style.logo}
+          />
           <div>
             <h3 className={style.h3}>{job.company}</h3>
             <span className={style.location}>
